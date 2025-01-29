b634d8285de5f9bbe66c7f9bf8a3cf72c61c1da6
yanet-y <yanetyohabeyene@gmail.com>
2025-01-29 09:49:15 +0300

    Minor changes

--- server.js
+++ server.js
@@ -9,19 +9,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const PORT= 5000;
+
 mongoose.connect(process.env.MONGO_URI, {
-  ssl: true,
-  tlsAllowInvalidCertificates: false,
 })
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB', err.message);
-    process.exit(1); 
   });
 
 
