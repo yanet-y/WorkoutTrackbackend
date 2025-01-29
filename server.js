472f9fe695b7e3573178719658f154c7f021df84
yanet-y <yanetyohabeyene@gmail.com>
2025-01-29 03:51:56 +0300

    Minor changes

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-require("dotenv").config();
-const express = require("express");
-const connectDB = require("./config/db");
-const cors = require("cors");
+require('dotenv').config(); 
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-
 
 app.use(express.json());
 app.use(cors());
 
-
-connectDB();
+mongoose.connect(process.env.MONGO_URI, {
+  ssl: true,
+  tlsAllowInvalidCertificates: false,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB', err.message);
+    process.exit(1); 
+  });
 
 
 app.use("/", require("./routes/authRoutes"));
 app.use("/workouts", require("./routes/workoutRoutes"));
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
