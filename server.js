ba5f9a0e900eb8770431beba247c203231b01c58
yanet-y <yanetyohabeyene@gmail.com>
2025-01-29 13:21:13 +0300

    Minor changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:5173',  
+  origin: '*',  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
