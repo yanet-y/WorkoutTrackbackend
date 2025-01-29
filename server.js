926132203454d705d844b2b1dda2763162dbd69a
yanet-y <yanetyohabeyene@gmail.com>
2025-01-29 10:00:46 +0300

    Minor changes

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-require('dotenv').config(); 
-
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-const PORT= 5000;
+const PORT = 5000; 
 
-mongoose.connect(process.env.MONGO_URI, {
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
+
+   
+    if (process.env.VERCEL !== "1") {
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    }
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB', err.message);
@@ -32,3 +33,6 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.path}`);
   next();
 });
+
+
+module.exports = app;
